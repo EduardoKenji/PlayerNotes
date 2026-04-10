af8c51022f9b188831b39a340795b8ab8504cfda
Eduardo Kenji <eduardo.koketu@gmail.com>
2026-04-10 19:47:49 -0300

    v2.0.0: Bump version

--- scripts/mods/PlayerNotes/PlayerNotes.lua
+++ scripts/mods/PlayerNotes/PlayerNotes.lua
@@ -1,7 +1,7 @@
 --[[
     PlayerNotes
     Author: Eduardo
-    Version: 1.9.12
+    Version: 2.0.0
 
     Add persistent notes to any player visible in the Social panel or Party Finder.
     Three simultaneous display mechanisms (each individually togglable via F4 Mod Options):
@@ -824,5 +824,5 @@ end)
 -- ──────────────────────────────────────────────────────────────────────────────
 
 mod.on_all_mods_loaded = function()
-    mod:echo("[PlayerNotes] v1.9.12 Loaded. /note /note_clear /pn_notes /pn_notes_delete_all")
+    mod:echo("[PlayerNotes] v2.0.0 Loaded. /note /note_clear /pn_notes /pn_notes_delete_all")
 end
